0a5a037831757c81c68344a51fc0c3f0201e2287
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-08-31 16:13:09 +0700

    add startUp infrastructure

---
+++ src/Infrastructure/Infrastructure/Startup.cs
@@ -0,0 +1,37 @@
+﻿using ECO.WebApi.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECO.WebApi.Infrastructure;
+public static class Startup
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services
+            .AddPersistence()
+            .AddRouting(options => options.LowercaseUrls = true);
+    }
+
+
+
+
+
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder builder) =>
+        builder
+            .UseRequestLocalization()
+            .UseStaticFiles()
+            .UseRouting()
+            .UseAuthentication()
+            .UseAuthorization();
+
+
+    public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapControllers().RequireAuthorization();
+        return builder;
+    }
+
+}
