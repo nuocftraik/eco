0a0a92bcd3b3fc6c407aa12e6712e7650d1f60a8
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-08-31 15:38:13 +0700

    add baseContext,applicationDbContext

---
+++ src/Infrastructure/Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ECO.WebApi.Infrastructure.Persistence.Context;
+public class ApplicationDbContext : BaseDbContext
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<DatabaseSettings> dbSettings)
+        : base(options, dbSettings)
+    {
+    }
+}
---
+++ src/Infrastructure/Infrastructure/Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,50 @@
+﻿using ECO.WebApi.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ECO.WebApi.Infrastructure.Persistence.Context;
+
+public abstract class BaseDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, ApplicationRoleClaim, IdentityUserToken<string>>
+{
+    private readonly IOptions<DatabaseSettings> _databaseSettings;
+    protected BaseDbContext(DbContextOptions options, IOptions<DatabaseSettings> dbSettings)
+    : base(options)
+    {
+        _databaseSettings = dbSettings;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // TODO: We want this only for development probably... maybe better make it configurable in logger.json config?
+        optionsBuilder.EnableSensitiveDataLogging();
+
+        // If you want to see the sql queries that efcore executes:
+
+        // Uncomment the next line to see them in the output window of visual studio
+        // optionsBuilder.LogTo(m => System.Diagnostics.Debug.WriteLine(m), Microsoft.Extensions.Logging.LogLevel.Information);
+
+        // Or uncomment the next line if you want to see them in the console
+        // optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+
+
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+
+        int result = await base.SaveChangesAsync(cancellationToken);
+
+
+
+        return result;
+    }
+}
+
