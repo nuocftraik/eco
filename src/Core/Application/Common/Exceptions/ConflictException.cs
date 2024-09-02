0cbda2318060ad19cb55f0eadef6f8abf32e1e55
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-03 00:04:24 +0700

    add appplication exception

---
+++ src/Core/Application/Common/Exceptions/ConflictException.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Net;
+
+
+namespace ECO.WebApi.Application.Common.Exceptions;
+public class ConflictException : CustomException
+{
+    public ConflictException(string message)
+        : base(message, null, HttpStatusCode.Conflict)
+    {
+    }
+}
---
+++ src/Core/Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Net;
+
+
+namespace ECO.WebApi.Application.Common.Exceptions;
+public class NotFoundException : CustomException
+{
+    public NotFoundException(string message)
+        : base(message, null, HttpStatusCode.NotFound)
+    {
+    }
+}
