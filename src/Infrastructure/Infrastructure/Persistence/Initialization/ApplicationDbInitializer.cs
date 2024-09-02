b217f1bd8db1f1ca1addf4d48c99e99746fbbe61
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-02 10:33:41 +0700

    seeding user ,role for system

--- src/Infrastructure/Infrastructure/Persistence/Initialization/ApplicationDbInitializer.cs
+++ src/Infrastructure/Infrastructure/Persistence/Initialization/ApplicationDbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using ECO.WebApi.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace ECO.WebApi.Infrastructure.Persistence.Initialization;
@@ -6,15 +7,31 @@ internal class ApplicationDbInitializer
 {
     private readonly ApplicationDbContext _dbContext;
     private readonly ILogger<ApplicationDbInitializer> _logger;
+    private readonly ApplicationDbSeeder _dbSeeder;
 
-    public ApplicationDbInitializer(ApplicationDbContext dbContext, ILogger<ApplicationDbInitializer> logger)
+    public ApplicationDbInitializer(ApplicationDbContext dbContext, ILogger<ApplicationDbInitializer> logger, ApplicationDbSeeder dbSeeder)
     {
         _dbContext = dbContext;
         _logger = logger;
+        _dbSeeder = dbSeeder;
     }
 
     public async Task InitializeAsync(CancellationToken cancellationToken)
     {
-       
+        if (_dbContext.Database.GetMigrations().Any())
+        {
+            if ((await _dbContext.Database.GetPendingMigrationsAsync(cancellationToken)).Any())
+            {
+                _logger.LogInformation("Applying Migrations for system");
+                await _dbContext.Database.MigrateAsync(cancellationToken);
+            }
+
+            if (await _dbContext.Database.CanConnectAsync(cancellationToken))
+            {
+                _logger.LogInformation("Connection to system's Database Succeeded.");
+
+                await _dbSeeder.SeedDatabaseAsync(_dbContext, cancellationToken);
+            }
+        }
     }
 }
