d34112f07b54bfbfd596c058edd9894cda706d73
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-02 22:39:47 +0700

    add IAggregateRoot

--- src/Core/Domain/Common/Contracts/BaseEntity.cs
+++ src/Core/Domain/Common/Contracts/BaseEntity.cs
@@ -12,8 +12,14 @@ public abstract class BaseEntity<TId> : IEntity<TId>
     public List<DomainEvent> DomainEvents { get; } = new();
 }
 
-
+//Sử dụng khi tất cả các thực thể trong hệ thống đều sử dụng Guid làm Id.
 public abstract class BaseEntity : BaseEntity<Guid>
 {
     protected BaseEntity() => Id = NewId.Next().ToGuid();
 }
+
+
+//Không giống như interface, abstract class cho phép triển khai sẵn
+//một số logic cơ bản,vd như việc quản lý list DomainEvents
+//hoặc tự động tạo Id. Nghĩa là các lớp con có thể kế thừa và sử dụng trực tiếp các logic này mà không cần phải tự triển khai lại.
+//Đồng thời ngăn cản việc khởi tạo trực tiếp các lớp cơ sở không đầy đủ thông tin hoặc logic để tồn tại độc lập.
--- src/Core/Domain/Common/Contracts/IEntity.cs
+++ src/Core/Domain/Common/Contracts/IEntity.cs
@@ -10,3 +10,12 @@ public interface IEntity<TId> : IEntity
 {
     TId Id { get; }
 }
+
+
+//IEntity định nghĩa rằng mọi (entity) trong hệ thống đều phải có một list(DomainEvents)
+//Nếu phát triển một hệ thống phức tạp, ví dụ như hệ thống thương mại điện tử,
+//nơi mỗi khi đơn hàng được tạo, cập nhật, hoặc hủy bỏ, bạn muốn các thành phần khác trong hệ thống(như hệ thống thông báo, hệ thống tồn kho) biết về các thay đổi này,
+//DomainEvents giúp bạn quản lý các sự kiện này.
+
+
+//IEntity<TId> mở rộng từ IEntity và thêm một thuộc tính Id, đại diện cho định danh duy nhất của thực thể. TId là kiểu dữ liệu tổng quát (generic type), cho phép Id có thể là bất kỳ kiểu dữ liệu nào (ví dụ: Guid, int, string, v.v.).
