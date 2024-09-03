06b049f5cd3726d104ded425a6c48c4d3c5e0520
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-03 16:52:40 +0700

    register repository

--- src/Infrastructure/Infrastructure/Persistence/Startup.cs
+++ src/Infrastructure/Infrastructure/Persistence/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using ECO.WebApi.Application.Common.Persistence;
+using ECO.WebApi.Domain.Common.Contracts;
 using ECO.WebApi.Infrastructure.Common;
 using ECO.WebApi.Infrastructure.Persistence.ConnectionString;
 using ECO.WebApi.Infrastructure.Persistence.Context;
 using ECO.WebApi.Infrastructure.Persistence.Initialization;
+using ECO.WebApi.Infrastructure.Persistence.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -38,7 +40,9 @@ internal static class Startup
             //.AddTransient(typeof(ICustomSeeder))
             .AddServices(typeof(ICustomSeeder), ServiceLifetime.Transient)
             .AddTransient<IConnectionStringSecurer, ConnectionStringSecurer>()
-            .AddTransient<IConnectionStringValidator, ConnectionStringValidator>();
+            .AddTransient<IConnectionStringValidator, ConnectionStringValidator>()
+
+            .AddRepositories();
     }
 
     internal static DbContextOptionsBuilder UseDatabase(this DbContextOptionsBuilder builder, string dbProvider, string connectionString) => dbProvider.ToLowerInvariant() switch
@@ -53,5 +57,28 @@ internal static class Startup
         //                      .SchemaBehavior(MySqlSchemaBehavior.Ignore)),
     };
 
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+
+        foreach (var aggregateRootType in
+            typeof(IAggregateRoot).Assembly.GetExportedTypes()
+                .Where(t => typeof(IAggregateRoot).IsAssignableFrom(t) && t.IsClass)
+                .ToList())
+        {
+            // Add ReadRepositories.
+            services.AddScoped(typeof(IReadRepository<>).MakeGenericType(aggregateRootType), sp =>
+                sp.GetRequiredService(typeof(IRepository<>).MakeGenericType(aggregateRootType)));
+
+            // Decorate the repositories with EventAddingRepositoryDecorators and expose them as IRepositoryWithEvents.
+            services.AddScoped(typeof(IRepositoryWithEvents<>).MakeGenericType(aggregateRootType), sp =>
+                Activator.CreateInstance(
+                    typeof(EventAddingRepositoryDecorator<>).MakeGenericType(aggregateRootType),
+                    sp.GetRequiredService(typeof(IRepository<>).MakeGenericType(aggregateRootType)))
+                ?? throw new InvalidOperationException($"Couldn't create EventAddingRepositoryDecorator for aggregateRootType {aggregateRootType.Name}"));
+        }
+
+        return services;
+    }
 
 }
