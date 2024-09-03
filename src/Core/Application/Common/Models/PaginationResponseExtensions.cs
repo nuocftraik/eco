153df40112306bfb62dfc45592eab22131d7fc71
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-03 15:17:27 +0700

    add pagination paramaters ,filter,search model

---
+++ src/Core/Application/Common/Models/PaginationResponseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+
+namespace ECO.WebApi.Application.Common.Models;
+public static class PaginationResponseExtensions
+{
+    public static async Task<PaginationResponse<TDestination>> PaginatedListAsync<T, TDestination>(
+        this IReadRepositoryBase<T> repository, ISpecification<T, TDestination> spec, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+        where T : class
+        where TDestination : class
+    {
+        var list = await repository.ListAsync(spec, cancellationToken);
+        int count = await repository.CountAsync(spec, cancellationToken);
+
+        return new PaginationResponse<TDestination>(list, count, pageNumber, pageSize);
+    }
+}
