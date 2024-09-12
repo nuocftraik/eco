d6f25d326cb12aa03df7d151055fed6bf5358cc4
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-12 22:23:53 +0700

    handle audit log before and after saveChange

--- src/Infrastructure/Infrastructure/Persistence/Context/BaseDbContext.cs
+++ src/Infrastructure/Infrastructure/Persistence/Context/BaseDbContext.cs
@@ -4,16 +4,27 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using ECO.WebApi.Infrastructure.Auditing;
+using ECO.WebApi.Application.Common.Interfaces;
+using ECO.WebApi.Domain.Common.Contracts;
+using ECO.WebApi.Shared.Events;
+using ECO.WebApi.Application.Common.Events;
 
 namespace ECO.WebApi.Infrastructure.Persistence.Context;
 
 public abstract class BaseDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, ApplicationRoleClaim, IdentityUserToken<string>>
 {
     private readonly IOptions<DatabaseSettings> _databaseSettings;
-    protected BaseDbContext(DbContextOptions options, IOptions<DatabaseSettings> dbSettings)
+    protected readonly ICurrentUser _currentUser;
+    private readonly ISerializerService _serializer;
+    private readonly IEventPublisher _events;
+
+    protected BaseDbContext(DbContextOptions options, IOptions<DatabaseSettings> dbSettings, ICurrentUser currentUser, ISerializerService serializer, IEventPublisher events)
     : base(options)
     {
         _databaseSettings = dbSettings;
+        _currentUser = currentUser;
+        _serializer = serializer;
+        _events = events;
     }
 
     public DbSet<Trail> AuditTrails => Set<Trail>();
@@ -43,11 +54,157 @@ public abstract class BaseDbContext : IdentityDbContext<ApplicationUser, Applica
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
     {
 
+        var auditEntries = HandleAuditingBeforeSaveChanges(_currentUser.GetUserId());
+
         int result = await base.SaveChangesAsync(cancellationToken);
 
+        await HandleAuditingAfterSaveChangesAsync(auditEntries, cancellationToken);
 
+        await SendDomainEventsAsync();
 
         return result;
+    }
+
+    private List<AuditTrail> HandleAuditingBeforeSaveChanges(Guid userId)
+    {
+        foreach (var entry in ChangeTracker.Entries<IAuditableEntity>().ToList())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedBy = userId;
+                    entry.Entity.LastModifiedBy = userId;
+                    break;
+
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedOn = DateTime.UtcNow;
+                    entry.Entity.LastModifiedBy = userId;
+                    break;
+
+                case EntityState.Deleted:
+                    if (entry.Entity is ISoftDelete softDelete)
+                    {
+                        softDelete.DeletedBy = userId;
+                        softDelete.DeletedOn = DateTime.UtcNow;
+                        entry.State = EntityState.Modified;
+                    }
+
+                    break;
+            }
+        }
+
+        ChangeTracker.DetectChanges();
+
+        var trailEntries = new List<AuditTrail>();
+        foreach (var entry in ChangeTracker.Entries<IAuditableEntity>()
+            .Where(e => e.State is EntityState.Added or EntityState.Deleted or EntityState.Modified)
+            .ToList())
+        {
+            var trailEntry = new AuditTrail(entry, _serializer)
+            {
+                TableName = entry.Entity.GetType().Name,
+                UserId = userId
+            };
+            trailEntries.Add(trailEntry);
+            foreach (var property in entry.Properties)
+            {
+                if (property.IsTemporary)
+                {
+                    trailEntry.TemporaryProperties.Add(property);
+                    continue;
+                }
+
+                string propertyName = property.Metadata.Name;
+                if (property.Metadata.IsPrimaryKey())
+                {
+                    trailEntry.KeyValues[propertyName] = property.CurrentValue;
+                    continue;
+                }
+
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        trailEntry.TrailType = TrailType.Create;
+                        trailEntry.NewValues[propertyName] = property.CurrentValue;
+                        break;
+
+                    case EntityState.Deleted:
+                        trailEntry.TrailType = TrailType.Delete;
+                        trailEntry.OldValues[propertyName] = property.OriginalValue;
+                        break;
+
+                    case EntityState.Modified:
+                        if (property.IsModified && entry.Entity is ISoftDelete && property.OriginalValue == null && property.CurrentValue != null)
+                        {
+                            trailEntry.ChangedColumns.Add(propertyName);
+                            trailEntry.TrailType = TrailType.Delete;
+                            trailEntry.OldValues[propertyName] = property.OriginalValue;
+                            trailEntry.NewValues[propertyName] = property.CurrentValue;
+                        }
+                        else if (property.IsModified && property.OriginalValue?.Equals(property.CurrentValue) == false)
+                        {
+                            trailEntry.ChangedColumns.Add(propertyName);
+                            trailEntry.TrailType = TrailType.Update;
+                            trailEntry.OldValues[propertyName] = property.OriginalValue;
+                            trailEntry.NewValues[propertyName] = property.CurrentValue;
+                        }
+
+                        break;
+                }
+            }
+        }
+
+        foreach (var auditEntry in trailEntries.Where(e => !e.HasTemporaryProperties))
+        {
+            AuditTrails.Add(auditEntry.ToAuditTrail());
+        }
+
+        return trailEntries.Where(e => e.HasTemporaryProperties).ToList();
+    }
+
+    private Task HandleAuditingAfterSaveChangesAsync(List<AuditTrail> trailEntries, CancellationToken cancellationToken = new())
+    {
+        if (trailEntries == null || trailEntries.Count == 0)
+        {
+            return Task.CompletedTask;
+        }
+
+        foreach (var entry in trailEntries)
+        {
+            foreach (var prop in entry.TemporaryProperties)
+            {
+                if (prop.Metadata.IsPrimaryKey())
+                {
+                    entry.KeyValues[prop.Metadata.Name] = prop.CurrentValue;
+                }
+                else
+                {
+                    entry.NewValues[prop.Metadata.Name] = prop.CurrentValue;
+                }
+            }
+
+            AuditTrails.Add(entry.ToAuditTrail());
+        }
+
+        return SaveChangesAsync(cancellationToken);
+    }
+
+    private async Task SendDomainEventsAsync()
+    {
+        var entitiesWithEvents = ChangeTracker.Entries<IEntity>()
+            .Select(e => e.Entity)
+            .Where(e => e.DomainEvents.Count > 0)
+            .ToArray();
+
+        foreach (var entity in entitiesWithEvents)
+        {
+            var domainEvents = entity.DomainEvents.ToArray();
+            entity.DomainEvents.Clear();
+            foreach (var domainEvent in domainEvents)
+            {
+                await _events.PublishAsync(domainEvent);
+            }
+        }
     }
 }
 
