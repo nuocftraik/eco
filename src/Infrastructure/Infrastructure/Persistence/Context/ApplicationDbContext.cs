2abb6d451a5483a39c28a89854525b6c5f05c015
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-09-12 22:23:53 +0700

    handle audit log before and after saveChange ,append global query filter for softDelete

--- src/Infrastructure/Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ src/Infrastructure/Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECO.WebApi.Application.Common.Events;
+using ECO.WebApi.Application.Common.Interfaces;
+using ECO.WebApi.Infrastructure.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace ECO.WebApi.Infrastructure.Persistence.Context;
 public class ApplicationDbContext : BaseDbContext
 {
-    public ApplicationDbContext(DbContextOptions options, IOptions<DatabaseSettings> dbSettings)
-        : base(options, dbSettings)
+    public ApplicationDbContext(DbContextOptions options, IOptions<DatabaseSettings> dbSettings, ICurrentUser currentUser, ISerializerService serializer, IEventPublisher events)
+        : base(options, dbSettings, currentUser, serializer, events)
     {
     }
+
+
 }
