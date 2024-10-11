8c97bdfa9093d0cd0faafbe903239631577d6c55
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-10-11 16:15:24 +0700

    search product request

---
+++ src/Core/Application/Catalog/Products/SearchProductRequest.cs
@@ -0,0 +1,55 @@
+﻿
+using ECO.WebApi.Domain.Enum;
+using Mapster;
+
+namespace ECO.WebApi.Application.Catalog.Products;
+public class SearchProductRequest : PaginationFilter, IRequest<PaginationResponse<ProductInListDto>>
+{
+    public DateRangeFilter PublishedFilter { get; set; }
+    public DateRangeFilter CreatedFilter { get; set; }
+    public DateRangeFilter ModifiedFilter { get; set; }
+    public ProductType? ProductType { get; set; }
+}
+
+public class SearchProductRequestValidator : AbstractValidator<SearchProductRequest>
+{
+    public SearchProductRequestValidator()
+    {
+        // Validate DateRangeFilters
+        RuleFor(x => x.PublishedFilter)
+            .IsInEnum().WithMessage("Invalid value for Published DateRangeFilter.");
+
+        RuleFor(x => x.CreatedFilter)
+            .IsInEnum().WithMessage("Invalid value for Created DateRangeFilter.");
+
+        RuleFor(x => x.ModifiedFilter)
+            .IsInEnum().WithMessage("Invalid value for Modified DateRangeFilter.");
+
+        RuleFor(x => x.ProductType)
+            .IsInEnum().WithMessage("Invalid value for ProductType.");
+
+    }
+}
+
+//Handler
+public class SearchProductRequestHandler : IRequestHandler<SearchProductRequest, PaginationResponse<ProductInListDto>>
+{
+    private readonly IRepository<Product> _repository;
+
+    public SearchProductRequestHandler(IRepository<Product> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<PaginationResponse<ProductInListDto>> Handle(SearchProductRequest request, CancellationToken cancellationToken)
+    {
+        var spec = new ProductBySearchSpec(request);
+        var totalCount = await _repository.CountAsync(spec, cancellationToken);
+        var data = await _repository.ListAsync(spec, cancellationToken);
+
+        var dtoData = data.Adapt<List<ProductInListDto>>();
+
+        return new PaginationResponse<ProductInListDto>(dtoData, totalCount, request.PageNumber, request.PageSize);
+    }
+
+}
---
+++ src/Core/Application/Catalog/Products/Specs/ProductBySearchSpec.cs
@@ -0,0 +1,50 @@
+﻿
+using ECO.WebApi.Domain.Enum;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECO.WebApi.Application.Catalog.Products;
+public class ProductBySearchSpec : EntitiesByPaginationFilterSpec<Product>
+{
+    public ProductBySearchSpec(SearchProductRequest request)
+         : base(request)
+    {
+        Query.Include(x => x.ProductCategories)
+             .ThenInclude(x => x.Category)
+             .OrderBy(c => c.CreatedOn, !request.HasOrderBy());
+        if (request.CreatedFilter != DateRangeFilter.All)
+        {
+            ApplyDateRangeFilter(request.CreatedFilter, "CreatedOn");
+        }
+
+        if (request.ModifiedFilter != DateRangeFilter.All)
+        {
+            ApplyDateRangeFilter(request.ModifiedFilter, "LastModifiedOn");
+        }
+
+        if (request.ProductType.HasValue)
+        {
+            Query.Where(c => c.ProductType == request.ProductType.Value);
+        }
+    }
+
+    private void ApplyDateRangeFilter(DateRangeFilter dateRange, string dateField)
+    {
+        DateTime now = DateTime.UtcNow;
+
+        switch (dateRange)
+        {
+            case DateRangeFilter.Last24Hours:
+                Query.Where(c => EF.Property<DateTime>(c, dateField) >= now.AddHours(-24));
+                break;
+            case DateRangeFilter.Last7Days:
+                Query.Where(c => EF.Property<DateTime>(c, dateField) >= now.AddDays(-7));
+                break;
+            case DateRangeFilter.Last30Days:
+                Query.Where(c => EF.Property<DateTime>(c, dateField) >= now.AddDays(-30));
+                break;
+            default:
+                break; // No filter for DateRange.All
+        }
+    }
+}
+
